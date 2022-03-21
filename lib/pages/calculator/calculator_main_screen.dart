b9b33c52c1b0cdeffe7f1854a1000fd64e1a6bfa
robson-rp <robson.paulo6060@gmail.com>
2022-03-21 16:35:59 +0100

    calculator_2

--- lib/pages/calculator/calculator_main_screen.dart
+++ lib/pages/calculator/calculator_main_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'button.dart';
 import 'package:math_expressions/math_expressions.dart';
 
 class CalculatorMainScreen extends StatefulWidget {
@@ -176,5 +175,55 @@ class _CalculatorMainScreenState extends State<CalculatorMainScreen> {
   void clear() {
     userAnswer = '';
     userQuestion = '';
+  }
+}
+
+class MyButton extends StatelessWidget {
+  const MyButton(
+      {Key? key,
+      required this.buttonText,
+      required this.textColor,
+      required this.buttonTapped,
+      required this.isDark})
+      : super(key: key);
+  final Color textColor;
+  final String buttonText;
+  final Function() buttonTapped;
+  final bool isDark;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: buttonTapped,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          decoration: BoxDecoration(
+              color: isDark ? Colors.grey[850] : Colors.grey[300],
+              shape: BoxShape.circle,
+              boxShadow: [
+                //Bottom shadow
+                BoxShadow(
+                    color: isDark ? Colors.grey[900]! : Colors.grey.shade400,
+                    offset: const Offset(2, 2),
+                    blurRadius: 4,
+                    spreadRadius: 2),
+                //top shadow
+                BoxShadow(
+                    color: isDark ? Colors.grey.shade800 : Colors.grey.shade200,
+                    offset: const Offset(-2, -2),
+                    blurRadius: 4,
+                    spreadRadius: 2)
+              ]),
+          child: Center(
+            child: Text(
+              buttonText,
+              style: TextStyle(
+                  color: textColor, fontWeight: FontWeight.bold, fontSize: 25),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
